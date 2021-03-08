55b79c8cb1035513ea1f2d9d72261eec54352a37
ad1012 <69478216+ad1012@users.noreply.github.com>
2021-03-07 19:02:51 -0600

    Add files via upload

---
+++ screens/in.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default class InScreen extends React.Component { 
+    render(){ 
+        return( 
+        <View style={styles.container}> 
+        <Text style={styles.textin}> Instagram </Text> 
+        </View> 
+        );
+     } 
+    } 
+    const styles = StyleSheet.create({ 
+        container: { 
+            flex:1, 
+            marginTop:200, 
+            backgroundColor: '#fff', 
+            alignItems: 'center', 
+            justifyContent: 'center', 
+        }, 
+        textin: { 
+            color:'lightpurple', 
+            fontWeight:"500", 
+            fontSize:20, 
+            alignItems: 'center', 
+            justifyContent: 'center', 
+        }, 
+    }) 
